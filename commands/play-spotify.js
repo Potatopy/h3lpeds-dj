10ac3bcd6cf44f884b489b5d2b4f683cd19ea275
Potatopy <baricaz001@outlook.com>
2022-11-11 12:15:34 -0500

    added spotify support

---
+++ commands/play-spotify.js
@@ -0,0 +1,116 @@
+// Credits: zach.#0001
+
+const {
+    SlashCommandBuilder,
+    EmbedBuilder
+} = require('discord.js')
+const { QueryType } = require("discord-player")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("playspotify")
+        .setDescription("plays songs from spotify")
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName("song")
+                .setDescription("Loads a single song from a url")
+                .addStringOption((option) => option.setName("url").setDescription("the song's url").setRequired(true))
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName("album")
+                .setDescription("Loads an album from url")
+                .addStringOption((option) => option.setName("url").setDescription("playlist url").setRequired(true))
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName("search")
+                .setDescription("Search for songs based on keyword")
+                .addStringOption((option) => option.setName("searchterms").setDescription("search for a song").setRequired(true)
+                )
+        )
+        .addSubcommand((subcommand) =>
+        subcommand
+            .setName("playlist")
+            .setDescription("Loads a playlist from spotify.")
+            .addStringOption((option) => option.setName("url").setDescription("playlist url").setRequired(true))
+        ),
+    run: async ({ client, interaction }) => {
+        if (!interaction.member.voice.channel)
+            return interaction.editReply("join a vc to make this command work")
+
+        const queue = await client.player.createQueue(interaction.guild)
+        if (!queue.connection) await queue.connect(interaction.member.voice.channel)
+
+        let embed = new EmbedBuilder()
+
+        if (interaction.options.getSubcommand() === "song") {
+            let url = interaction.options.getString("url")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.SPOTIFY_SONG
+            })
+            if (result.tracks.length === 0)
+                return interaction.editReply("No Results")
+
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}] (${song.url})** has been added to the Queue!`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration} made by zach.#0001` })
+
+        } else if (interaction.options.getSubcommand() === "playlist") {
+            let url = interaction.options.getString("url")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.SPOTIFY_ALBUM
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.editReply("No Results. Make sure the playlist is public!")
+
+            const playlist = result.playlist
+            await queue.addTracks(result.tracks)
+            embed
+                .setDescription(`**${result.tracks.length} songs from [${playlist.title}] (${playlist.url})** has been added to the Queue!`)
+                .setFooter({ text: 'Made by zach.#0001' })
+        } else if (interaction.options.getSubcommand() === "search") {
+            let url = interaction.options.getString("searchterms")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.AUTO
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.editReply("No Results. Make sure the playlist is public!")
+
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}] (${song.url})** has been added to the Queue!`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration} made by zach.#0001` })
+        } else if (interaction.options.getSubcommand() === "playlist") {
+            let url = interaction.options.getString("url")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.SPOTIFY_PLAYLIST
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.editReply("No Results. Make sure the playlist is public!")
+
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}] (${song.url})** has been added to the Queue!`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration} made by zach.#0001` })
+        }
+        if (!queue.playing) await queue.play()
+        await interaction.editReply({
+            embeds: [embed]
+        })
+    },
+}
